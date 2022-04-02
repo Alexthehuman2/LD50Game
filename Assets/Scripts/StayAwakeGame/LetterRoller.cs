caa9d5db2a8d7ebef982752ef361ff93fe263594
Ace Zhang <acezhang18@gmail.com>
2022-04-02 15:47:39 -0400

    deletion of duplicate gamecontroller

--- Assets/Scripts/StayAwakeGame/LetterRoller.cs
+++ Assets/Scripts/StayAwakeGame/LetterRoller.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class LetterRoller : MonoBehaviour
 {
+    private char letter;
     // Start is called before the first frame update
     void Start()
     {
-        
+        letter = RollLetter();
     }
 
     // Update is called once per frame
