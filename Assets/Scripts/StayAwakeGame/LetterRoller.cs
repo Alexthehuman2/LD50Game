d5bfeb268d9eff37e71d363b83b1b42e342988fb
Ace Zhang <acezhang18@gmail.com>
2022-04-02 15:57:50 -0400

    added key check

--- Assets/Scripts/StayAwakeGame/LetterRoller.cs
+++ Assets/Scripts/StayAwakeGame/LetterRoller.cs
@@ -14,7 +14,113 @@ public class LetterRoller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.A)){
+
+        }
+        else if (Input.GetKeyDown(KeyCode.B))
+        {
+
+        }
+        else if (Input.GetKeyDown(KeyCode.C))
+        {
+
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+
+        }
+        else if (Input.GetKeyDown(KeyCode.E))
+        {
+
+        }
+        else if (Input.GetKeyDown(KeyCode.E))
+        {
+
+        }
+        else if (Input.GetKeyDown(KeyCode.F))
+        {
+
+        }
+        else if (Input.GetKeyDown(KeyCode.G))
+        {
+
+        }
+        else if (Input.GetKeyDown(KeyCode.H))
+        {
+
+        }
+        else if (Input.GetKeyDown(KeyCode.I))
+        {
+
+        }
+        else if (Input.GetKeyDown(KeyCode.J))
+        {
+
+        }
+        else if (Input.GetKeyDown(KeyCode.K))
+        {
+
+        }
+        else if (Input.GetKeyDown(KeyCode.L))
+        {
+
+        }
+        else if (Input.GetKeyDown(KeyCode.M))
+        {
+
+        }
+        else if (Input.GetKeyDown(KeyCode.N))
+        {
+
+        }
+        else if (Input.GetKeyDown(KeyCode.O))
+        {
+
+        }
+        else if (Input.GetKeyDown(KeyCode.P))
+        {
+
+        }
+        else if (Input.GetKeyDown(KeyCode.Q))
+        {
+
+        }
+        else if (Input.GetKeyDown(KeyCode.R))
+        {
+
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+
+        }
+        else if (Input.GetKeyDown(KeyCode.T))
+        {
+
+        }
+        else if (Input.GetKeyDown(KeyCode.U))
+        {
+
+        }
+        else if (Input.GetKeyDown(KeyCode.V))
+        {
+
+        }
+        else if (Input.GetKeyDown(KeyCode.W))
+        {
+
+        }
+        else if (Input.GetKeyDown(KeyCode.X))
+        {
+
+        }
+        else if (Input.GetKeyDown(KeyCode.Y))
+        {
+
+        }
+        else if (Input.GetKeyDown(KeyCode.Z))
+        {
+
+        }
     }
 
     public char RollLetter()
