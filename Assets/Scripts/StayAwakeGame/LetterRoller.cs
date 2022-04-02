b3c495e91db0a215f465b788c8ad0d1786394b8b
Ace Zhang <acezhang18@gmail.com>
2022-04-02 15:46:46 -0400

    added letter roller

---
+++ Assets/Scripts/StayAwakeGame/LetterRoller.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LetterRoller : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public char RollLetter()
+    {
+        Random random = new Random();
+        // random lowercase letter
+        int a = Random.Range(0, 26);
+        char ch = (char)('a' + a);
+
+        return ch;
+    }
+}
