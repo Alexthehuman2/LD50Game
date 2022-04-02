389da61169e02203735a72a390b4d95566ca6a45
Alex Wai Yiu Wong <aw987123@gmail.com>
2022-04-02 15:57:04 +0100

    Ice Cream Game without Control with fail states done

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public static GameController Instance { get; private set; }
+
+    public int score { get; private set; }
+
+    public void setScore(int new_score)
+    {
+        score = new_score;
+    }
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+
+    private void OnDestroy()
+    {
+        if (this == Instance)
+        {
+            Debug.Log("Destroying a GameController");
+        }
+    }
+}
---
+++ Assets/Scripts/MoveIceCreamRandomly.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveIceCreamRandomly : MonoBehaviour
+{
+
+    [SerializeField] private float pushForce = 5f;
+    [SerializeField] private Rigidbody2D iceCreamRB;
+
+    [SerializeField] private GameObject iceCreamCone;
+    [SerializeField] private GameObject floor;
+
+    [SerializeField] private bool fallingOff;
+
+    private int score = 0;
+
+    private void Start()
+    {
+        iceCreamRB = this.GetComponent<Rigidbody2D>();
+        score = GameController.Instance.score;
+        StartCoroutine(increaseScore());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!fallingOff)
+        {
+            //rigidbody2d add force to an axis randomly between clamped to -1 to 1, multiplied by pushforce
+            float randX = Random.Range(-10, 10);
+            Vector2 newForce = new Vector2(randX / 10, 0);
+            iceCreamRB.AddForce(newForce * pushForce);
+        }
+    }
+
+    IEnumerator increaseScore()
+    {
+        while (!fallingOff)
+        {
+            yield return new WaitForSeconds(0.5f);
+            score++;
+        }
+        Debug.Log("Final Score: " + score);
+        GameController.Instance.setScore(score);
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject == iceCreamCone)
+        {
+            fallingOff = true;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject == floor)
+        {
+            Debug.Log("Game Over");
+            //GameOver, show score, back to menu screen.
+        }
+    }
+}
