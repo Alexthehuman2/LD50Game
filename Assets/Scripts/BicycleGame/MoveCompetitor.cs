d1b689c0234f6b0218474845ccde8616a9548ca9
Ace Zhang <acezhang18@gmail.com>
2022-04-03 12:36:30 -0400

    adjusted values

--- Assets/Scripts/BicycleGame/MoveCompetitor.cs
+++ Assets/Scripts/BicycleGame/MoveCompetitor.cs
@@ -5,16 +5,14 @@ using UnityEngine;
 public class MoveCompetitor : MonoBehaviour
 {
     [SerializeField] private float baseSpeed;
-    private GameController control;
     // Start is called before the first frame update
     void Start()
     {
-        control = GameObject.Find("GameController").GetComponent<GameController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        gameObject.transform.GetComponent<Rigidbody2D>().velocity += new Vector2(1 + (baseSpeed * control.score)/200, 0);
+        gameObject.transform.GetComponent<Rigidbody2D>().velocity += new Vector2((0.015f + (baseSpeed * GameController.Instance.score) * Time.deltaTime), 0);
     }
 }
