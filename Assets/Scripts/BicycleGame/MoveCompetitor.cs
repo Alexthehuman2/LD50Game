03e4bca8c54b202e298b1db4996c08d943d6a866
Ace Zhang <acezhang18@gmail.com>
2022-04-03 12:12:09 -0400

    competitor tracks

---
+++ Assets/Scripts/BicycleGame/MoveCompetitor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCompetitor : MonoBehaviour
+{
+    [SerializeField] private float baseSpeed;
+    private GameController control;
+    // Start is called before the first frame update
+    void Start()
+    {
+        control = GameObject.Find("GameController").GetComponent<GameController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.GetComponent<Rigidbody2D>().velocity += new Vector2(baseSpeed * control.score, 0);
+    }
+}
