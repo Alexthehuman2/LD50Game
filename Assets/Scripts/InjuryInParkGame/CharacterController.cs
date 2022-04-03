a8aeac7702c6e5506f3ba5fefe0d9b5608200996
Alex Wai Yiu Wong <aw987123@gmail.com>
2022-04-03 18:49:18 +0100

    Wow Ice Cream Game Win Loss Panel Bugged what is this

--- Assets/Scripts/InjuryInParkGame/CharacterController.cs
+++ Assets/Scripts/InjuryInParkGame/CharacterController.cs
@@ -2,21 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum RotationState
-{
-    LEFT = 0,
-    RIGHT = 1
-}
+
 public class CharacterController : MonoBehaviour
 {
-    private RotationState state;
     private Vector3 playerRot;
+    private bool rotatingLeft = true;
+    private float currentZ = 0;
     [SerializeField] private GameObject children;
     [SerializeField] private GameObject scoreScript;
     [SerializeField] private GameObject winLossPanel;
     [SerializeField] private float fallThresholdInDegrees = 75f;
     [SerializeField] private float fallSpeed = 10f;
     [SerializeField] private float fallSpeedCap = 15f;
+    [SerializeField] private float playerRotInfluence = 12.5f;
 
     private void Start()
     {
@@ -25,16 +23,50 @@ public class CharacterController : MonoBehaviour
 
     private void Update()
     {
+        //gets the player rotation as variable
         playerRot = children.transform.rotation.eulerAngles;
+
+        //Decides rotation state whether to increase left or right
+        rotatingLeft = (playerRot.z >= 0);
+        Debug.Log("Rotating Left? : " + rotatingLeft);
+        
         if (GameController.Instance.canMove)
         {
-            //insert rotation logic here
-            Vector3 newRot = new Vector3(0f, 0f, fallSpeed*Time.deltaTime);
-            children.transform.rotation = Quaternion.Euler(playerRot+newRot);
+            //Rotation Polish: LERP the rot speed to cap
+            float rotZ = rotatingLeft == true ? fallSpeed : -fallSpeed; //this get influenced by player input
+            currentZ += rotZ;
+            if (currentZ > fallSpeedCap)
+            {
+                currentZ = fallSpeedCap;
+            }
+            else if (currentZ < -fallSpeedCap)
+            {
+                currentZ = -fallSpeedCap;
+            }
+
+            float threshold = rotatingLeft == true ? fallThresholdInDegrees : -fallThresholdInDegrees;
+
+            //player input
+            if (Input.GetKey(KeyCode.D))
+            {
+                currentZ -= playerRotInfluence;
+                Debug.Log("Pressing D, current RotZ: "+rotZ);
+            }
+            else if (Input.GetKey(KeyCode.A))
+            {
+                currentZ += playerRotInfluence;
+                Debug.Log("Pressing A, current RotZ: " + rotZ);
+            }
+            Vector3 newRot = new Vector3(0f, 0f, currentZ*Time.deltaTime);
+
+            children.transform.rotation = Quaternion.Euler(playerRot+newRot); //endgoal
+            //A value that gets added to the rotation, the value is lerped to an influenced value by player holding down key, and the update loop generating rotation
         }
+        playerRot = children.transform.rotation.eulerAngles;
 
         //if the player falls over, this happens
         //POLISH: Make it so it only happens when it touches the ground and rotation is beyond threshold.
+        // -1, 359
         if ((playerRot.z > fallThresholdInDegrees || playerRot.z < -fallThresholdInDegrees) && GameController.Instance.canMove)
         {
             Debug.Log("This is running");
@@ -46,4 +78,5 @@ public class CharacterController : MonoBehaviour
             winLossPanel.SetActive(true);
         }
     }
+
 }
