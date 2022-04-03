658705fae649475c0f6fa591625aa8a06cb79719
Ace Zhang <acezhang18@gmail.com>
2022-04-03 12:52:27 -0400

    UI changes

---
+++ Assets/Scripts/BicycleGame/GoalController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalController : MonoBehaviour
+{
+    [SerializeField] private WinLossUI winloss;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            winloss.setState(WinLossState.WIN);
+        }
+        else
+        {
+            winloss.setState(WinLossState.LOSS);
+        }
+    }
+}
